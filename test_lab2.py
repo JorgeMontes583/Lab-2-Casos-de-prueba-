8652f3fb3606705c507a901a00637f9cc072cfd4
JorgeMontes583 <jorgemontes083@gmail.com>
2026-09-11 01:37:43 +0000

    Practica 3 completada

--- test_lab2.py
+++ test_lab2.py
@@ -1,17 +1,56 @@
 import ecommerce_form
 import logging
+import pytest 
 
 logging.basicConfig(
     level= logging.DEBUG,
     filename= 'test.log',
     filemode='w'
 )
+@pytest.fixture
+def system():
+    return ecommerce_form.OnlinePurchase()
 
-def test_purchase_itemzero():
+@pytest.mark.parametrize('quantity, expected',[
+    (8,True),
+    (0, False),
+    (-1, False),
+    (.8, False)
+    ])
+@pytest.mark.unit
+def test_validate_quantity_component(system, quantity, expected):
+    result=system.validate_quantity(quantity)
+    assert result == expected
+
+####
+
+@pytest.mark.parametrize('coupon, expected',[
+    ('DISCOUNT10',True),
+    ('DISCOUNT20', True),
+    ('DISCOUNT30', False),
+    ('DISCOUNT40', False)
+    ])
+@pytest.mark.unit
+def test_validate_coupon(system, coupon, expected):
+    result=system.validate_coupon(coupon)
+    assert result == expected
+###
+
+@pytest.mark.parametrize('address, expected',[
+    ('av patria',True),
+    ('dos', False),
+    ('332222323', False)
+    ])
+@pytest.mark.wip
+def test_validate_address(system, address, expected):
+    result=system.validate_address(address)
+    assert result == expected
+
+@pytest.mark.system
+def test_purchase_itemzero(system):
     logging.info('TestCase 1')
            
-    system = ecommerce_form.OnlinePurchase()
-
+    #system = ecommerce_form.OnlinePurchase()
     cart ={
         'Laptop': 0,
         'Mouse': 2
@@ -25,6 +64,7 @@ def test_purchase_itemzero():
     logging.info (f'Result:{result}')
     logging.info('TestCase FINISHED')
 
+@pytest.mark.system
 def test_coupon_invalid():
     logging.info('TestCase 2 (RF3)')
     system = ecommerce_form.OnlinePurchase()
@@ -39,6 +79,7 @@ def test_coupon_invalid():
     assert 'code is not valid' in result
     logging.info('TestCase 2 FINISHED')
 
+@pytest.mark.system
 def test_coupon_valid():
     logging.info('TestCase 3 (RF9)')
     system = ecommerce_form.OnlinePurchase()
